d861752d9eb856e0f0c7963167aaa93d2b99000b
Andrew Nguyen <66585882+Oddahere@users.noreply.github.com>
2023-11-19 23:27:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose') //require mongoose library functionaility
-const morgan = require('morgan') // better debugging
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes/route');
+const router = express.Router();
+// Database Connection
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.DB,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DBNAME,
+    dialect: process.env.DIALECT
+});
+
+const app = express();
 
 
-const cors = require('cors')
-// allow using a .env file
-require('dotenv').config() //require the dotenv
+app.use('/', routes);
+app.use(express.json()); 
+app.use(morgan('dev'));
 
-// creates a new instance of express application
-const app = express()
 
-// add cors header to the server
-app.use(
-  cors({
-    origin: '*'
-  })
-)
-
-// sets up mongoose for the mongoDB connection
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log('Database connection Success!')
-  })
-  .catch((err) => {
-    console.error('Mongo Connection Error', err)
-  })
-
-// declare port number for the api
-const PORT = process.env.PORT || 4500
-
-// setup and access request body
-app.use(express.json())
-app.use(morgan('dev'))
-
-// setup middle ware for routes
-app.use('/drivers', require('./routes/drivers'))
-app.use('/orders', require('./routes/orders'))
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // logs error and error code to console
-  console.error(err.message, req)
-  if (!err.statusCode) {
-    err.statusCode = 500
-  }
-  res.status(err.statusCode).send(err.message)
-})
+// Express Listener
+const listenerPort = process.env.LISTENER || 3000; // Default to 3000 if LISTENER is not set
+app.listen(listenerPort, () => {
+  console.log(`Server is running on port ${listenerPort}`);
+});
+ 
+// Rest of your express app logic
